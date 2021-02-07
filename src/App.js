230d751b2c773e1074ad14e47ecac05c3c88dab3
akashghanate <akashghanate98@gmail.com>
2021-02-07 17:48:28 +0530

    added fileView logic and css, +  firestore db

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import Header from './components/header';
-import Sidebar from './components/sidebar'
+import "./App.css";
+import FilesView from "./components/filesView/FilesView";
+import Header from "./components/header";
+import Sidebar from "./components/sidebar";
 
 function App() {
   // Authentication
 
-
-
   return (
     <div className="App">
       <Header />
-      <Sidebar/>
-     {/* if auth: true
+      <div className="app__main">
+        <Sidebar />
+        <FilesView />
+      </div>
+      {/* if auth: true
       display: header, sidebar, RightsideIcons, filesView */}
 
-     {/* not authenticated: log-in */}
+      {/* not authenticated: log-in */}
     </div>
   );
 }
